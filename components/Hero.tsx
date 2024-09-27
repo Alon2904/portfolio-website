56f6408fd13d0fb12628bfbd96b5cbac6b062bb7
Alon Cohen <21098015+Alon2904@users.noreply.github.com>
2024-09-27 15:42:17 +0100

    Refactor Hero component to use animated section and update styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,21 @@
-// components/Hero.tsx
-import React from 'react';
+import React from "react";
+import { heroText } from "@/utils/heroText";
+import { playfair } from "@/utils/fonts";
+import AnimatedSection from "./layout/AnimatedSection";
 
-const Hero: React.FC = () => (
-  <section id="home" className="min-h-screen flex items-center justify-center">
-    <div className="w-full max-w-[1000px] mx-auto text-center">
-      <h1 className="text-5xl font-bold">Welcome to My Portfolio</h1>
-    </div>
-  </section>
-);
+const Hero = () => {
+  return (
+    <AnimatedSection
+      className="relative flex flex-col md:justify-center min-h-[50vh] justify-center lg:min-h-hero p-10 md:pe-56  md:bg-inherit text-primary-contant text-center md:text-left "
+      id="home">
+      <h1
+        className={`text-5xl md:text-8xl font-bold mb-2 z-30 ${playfair.className}`}>
+        {heroText.title}
+      </h1>
+      <h2 className="text-xl md:text-2xl mb-4">{heroText.subtitle}</h2>
+      <p className="text-lg md:text-xl font-light">{heroText.description}</p>
+    </AnimatedSection>
+  );
+};
 
 export default Hero;
