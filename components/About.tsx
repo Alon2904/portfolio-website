567e1cd86a9f3cd4378e07bbae9d99b4508d21d0
Alon Cohen <21098015+Alon2904@users.noreply.github.com>
2024-09-27 16:43:55 +0100

    Refactor About component to use animated section and update styling

--- components/About.tsx
+++ components/About.tsx
@@ -1,16 +1,50 @@
-// components/About.tsx
+import React from "react";
+import { aboutMe } from "@/utils/aboutMe";
+import { playfair } from "@/utils/fonts";
+import { techStack } from "@/utils/techStack";
+import TechIcon from "@/components/stackIcon";
+import { IconName } from "@fortawesome/fontawesome-svg-core";
+import AnimatedSection from "./layout/AnimatedSection";
+import { FaChevronDown } from "react-icons/fa";
+import Link from "next/link";
 
-import React from 'react';
-
-const About: React.FC = () => (
-  <section id="about" className="py-20 bg-white">
-    <div className="container mx-auto px-4">
-      <h2 className="text-4xl font-bold text-center">About Me</h2>
-      <p className="mt-8 text-lg text-center max-w-2xl mx-auto">
-        [Your background, skills, and interests.]
+const AboutMe = () => {
+  const iconStyle =
+    "transform hover:scale-110 transition-transform w-10 h-10 hover:text-primary";
+  return (
+    <AnimatedSection
+      className="flex flex-col justify-center min-h-[70vh] lg:min-h-[80vh] p-10 md:pe-56"
+      id="about">
+      <h1
+        className={`text-4xl md:text-6xl font-bold mb-2 ${playfair.className}`}>
+        About Me.
+      </h1>
+      <p className="text-lg md:text-xl font-light mb-4">
+        {aboutMe.description}
       </p>
-    </div>
-  </section>
-);
 
-export default About;
+      <h2 className={`text-lg md:text-xl font-medium mb-2`}>
+        {`Technologies I've worked with:`}
+      </h2>
+      <div className="grid grid-cols-4 md:flex md:flex-wrap gap-4">
+        {techStack.map(({ name, iconName }) => (
+          <TechIcon
+            key={name}
+            iconName={iconName as IconName}
+            name={name}
+            style={iconStyle}
+          />
+        ))}
+      </div>
+      <Link
+        href="https://drive.google.com/file/d/149B5bTah7nQV3ir3-l-6wIT4dG5sRlCQ/view?usp=sharing"
+        target="_blank"
+        className="btn md:btn-wide btn-primary text-primary-content font-normal mx-auto mt-8 md:mt-14">
+        Download CV
+        <FaChevronDown />
+      </Link>
+    </AnimatedSection>
+  );
+};
+
+export default AboutMe;
