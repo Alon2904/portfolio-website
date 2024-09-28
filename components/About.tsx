f8f9d13c8cbe8608ce463217516c1ac32b4e1ed4
Alon Cohen <21098015+Alon2904@users.noreply.github.com>
2024-09-28 11:28:17 +0100

    Refactor AboutMe component to update profile photo styling and add download CV button

--- components/About.tsx
+++ components/About.tsx
@@ -11,17 +11,30 @@ import Link from "next/link";
 const AboutMe = () => {
   const iconStyle =
     "transform hover:scale-110 transition-transform w-10 h-10 hover:text-primary";
+
   return (
     <AnimatedSection
       className="flex flex-col justify-center min-h-[70vh] lg:min-h-[80vh] p-10 md:pe-56"
-      id="about">
-      <h1
-        className={`text-4xl md:text-6xl font-bold mb-2 ${playfair.className}`}>
-        About Me.
-      </h1>
-      <p className="text-lg md:text-xl font-light mb-4">
-        {aboutMe.description}
-      </p>
+      id="about"
+    >
+      <div className="flex flex-row justify-between w-full">
+        <h1
+          className={`text-4xl md:text-6xl font-bold mb-2 ${playfair.className}`}
+        >
+          About Me.
+        </h1>
+        <Link
+          href="https://drive.google.com/file/d/149B5bTah7nQV3ir3-l-6wIT4dG5sRlCQ/view?usp=sharing"
+          target="_blank"
+          className="mx-auto flex flex-col items-center justify-end mb-2 lg:justify-center lg:mb-0" // Align at the top
+        >
+          <button className="btn flex items-center justify-center md:btn-wide btn-primary text-primary-content font-normal shadow px-4 py-2 rounded-full transition-colors duration-300 hover:bg-black hover:text-white">
+            <span className="mr-2">Download CV</span>
+            <FaChevronDown />
+          </button>
+        </Link>
+      </div>
+      <p className="text-lg md:text-xl font-light mb-4">{aboutMe.description}</p>
 
       <h2 className={`text-lg md:text-xl font-medium mb-2`}>
         {`Technologies I've worked with:`}
@@ -36,13 +49,6 @@ const AboutMe = () => {
           />
         ))}
       </div>
-      <Link
-        href="https://drive.google.com/file/d/149B5bTah7nQV3ir3-l-6wIT4dG5sRlCQ/view?usp=sharing"
-        target="_blank"
-        className="btn md:btn-wide btn-primary text-primary-content font-normal mx-auto mt-8 md:mt-14">
-        Download CV
-        <FaChevronDown />
-      </Link>
     </AnimatedSection>
   );
 };
