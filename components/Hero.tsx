afcd8cf518464d369f430e3a0a74217b8873844f
Alon Cohen <21098015+Alon2904@users.noreply.github.com>
2024-09-28 11:28:25 +0100

    Refactor Hero component to update profile photo styling and add typing animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,23 +1,35 @@
 'use client';
-import React from "react";
-import { ReactTyped } from "react-typed";
+import React, { useState, useEffect } from "react";
+import { ReactTyped, Typed } from "react-typed";
 import { heroText } from "@/utils/heroText";
 import { playfair } from "@/utils/fonts";
 import AnimatedSection from "./layout/AnimatedSection";
 
 const Hero = () => {
+  const [typed, setTyped] = useState<Typed | undefined>();
+
+
   return (
     <AnimatedSection
-      className="relative flex flex-col md:justify-center min-h-[50vh] justify-center lg:min-h-hero p-10 md:pe-56 md:bg-inherit text-primary-content text-center md:text-left"
+      className="relative flex flex-col md:justify-center min-h-screen justify-center lg:min-h-hero p-10 md:pe-56 md:bg-inherit text-primary-content text-center md:text-left"
       id="home"
     >
       <h1
         className={`text-5xl md:text-8xl font-bold mb-2 z-30 ${playfair.className}`}
       >
         <ReactTyped
-          strings={[heroText.title]} // Make sure heroText.title is a string or an array of strings
-          typeSpeed={40}
-          backSpeed={50}
+          typedRef={setTyped}
+          strings={[heroText.title]} // Ensure heroText.title is a string or an array of strings
+          typeSpeed={100}
+          backSpeed={100}
+          showCursor={true} // Show blinking cursor
+          cursorChar="|" // Set the cursor character
+            onComplete={(self) => {
+            setTimeout(() => {
+              self.cursor.remove(); // Remove the cursor when typing is done
+            }, 1500); // Wait for 2 seconds before removing the cursor
+            }}
+      
         />
       </h1>
       <h2 className="text-xl md:text-2xl mb-4">{heroText.subtitle}</h2>
