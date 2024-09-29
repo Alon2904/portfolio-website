cab463edd5dfade05dba8207595fa968808b8766
Alon Cohen <21098015+Alon2904@users.noreply.github.com>
2024-09-29 22:23:40 +0100

    Refactor layout component to add metadata for SEO optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,17 @@ import { poppins } from "@/utils/fonts";
 import LayoutWrapper from "@/components/layout/LayoutWrapper";
 import Head from "next/head";
 
+
+export const metadata = {
+  title: "Alon Cohen's Portfolio",
+  description: "Alon's Portfolio",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png", // Optional: For Apple devices
+    shortcut: "/favicon.ico", // Optional: Legacy favicon
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -10,14 +21,6 @@ export default function RootLayout({
 }>) {
   return (
     <html>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <title>Alon Cohen&apos;s Portfolio</title>{" "}
-        {/* Escaped the single quote */}
-        <meta name="description" content="Alon's Portfolio" />
-      </Head>
       <body className={poppins.className}>
         <main className="flex flex-col md:flex-row relative">
           <section className="md:w-7/12 flex flex-col">
@@ -28,3 +31,5 @@ export default function RootLayout({
     </html>
   );
 }
+
+
