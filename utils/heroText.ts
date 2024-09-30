45e9e1fba787300b30017878cd6fa7dba11c65c6
Alon Cohen <21098015+Alon2904@users.noreply.github.com>
2024-09-30 16:01:49 +0100

    Refactor heroText description in utils/heroText.ts

--- utils/heroText.ts
+++ utils/heroText.ts
@@ -1,7 +1,7 @@
 export const heroText = {
     title: "Alon Cohen;",
     subtitle: "Full-Stack Software Engineer",
-    description: "Full-Stack Software Engineer with 3 years of experience in full-stack development. I specialize in building web applications and cloud solutions.\nI'm passionate about creating innovative solutions that help businesses grow.",
+    description: "Full-Stack Software Engineer with 3 years of experience in full-stack development. I specialize in building web applications and cloud solutions.\nI'm passionate about creating innovative solutions that help businesses to grow.",
   };
   
   
